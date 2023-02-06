d432f552aeab5d4ef7cb762e3fd8da62935d586d
ritikwan <113706704+ritikwan@users.noreply.github.com>
2023-02-06 20:11:40 +0530

    ok

---
+++ hello.py
@@ -0,0 +1,10 @@
+import sys
+from termcolor import colored, cprint
+
+text = colored("hello world","red",attrs=["reverse","blink"])
+
+print(text)
+print(colored("this is yrllow color",'yellow'))
+
+
+
